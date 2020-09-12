2a34c3b03eb271c526be64d0d417cde5cbb96abc
juanfer2 <jfvilladiego3@gmail.com>
2020-09-12 16:32:15 -0500

    save cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,34 @@
-import React,{useEffect} from 'react';
+import React, { useEffect } from 'react'
 /*Redux */
 import { useDispatch, useSelector } from 'react-redux'
-import logo from './logo.svg';
-import './App.css';
-import {env} from './contants/api.constant'
-
+import logo from './logo.svg'
+import './App.css'
+import { env } from './contants/api.constant'
+import { createMuiTheme } from '@material-ui/core/styles'
 /** Actions */
-import {healtAction} from './flux/actions/healt.action'
+import { healtAction } from './flux/actions/healt.action'
 
 import Navbar from './components/navbar'
+import Home from './components/home'
+import Pokemons from './components/pokemons'
 
-function App () {
-
-  const dispatch = useDispatch();
-  const state = useSelector( state => state )
+function App() {
+  const dispatch = useDispatch()
+  const state = useSelector((state) => state)
 
   useEffect(() => {
-    const  startHealtDispacht = () =>  dispatch(healtAction( ));
+    const startHealtDispacht = () => dispatch(healtAction())
     startHealtDispacht()
-    console.log(env);
+    console.log(env)
   }, [])
   console.log(state)
 
-
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-          {process.env.REACT_APP_ENVIROMENT}
-        </p>
-        <Navbar />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Home />
+      <Pokemons />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,30 @@
-import React,{Suspense} from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import './styles/main.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { Suspense } from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import { theme } from './index.styles'
+import { ThemeProvider } from '@material-ui/styles'
+import './styles/main.css'
 
-import { Provider } from "react-redux";
-import store from "./flux/store";
+import App from './App'
+import * as serviceWorker from './serviceWorker'
+
+import { Provider } from 'react-redux'
+import store from './flux/store'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Suspense fallback={"loanding..."}>
-      <Provider store={store}>
-        <App /> 
-      </Provider>
-    </Suspense>
+    <ThemeProvider theme={theme}>
+      <Suspense fallback={'loanding...'}>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </Suspense>
+    </ThemeProvider>
   </React.StrictMode>,
-  document.getElementById('root')
-);
+  document.getElementById('root'),
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
